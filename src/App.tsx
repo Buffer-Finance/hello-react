19f681bab1c5a1f4c5ae8a29f4c61bc3c17ff6b1
maxwell-1999 <maxwell@buffer.finance>
2023-09-14 16:26:28 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
 import React, { useCallback } from 'react'
-import styled from 'styled-components'
-import { Button, Title } from '@gnosis.pm/safe-react-components'
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
-
-const Container = styled.div`
-  padding: 1rem;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`
-
-const Link = styled.a`
-  margin-top: 8px;
-`
-
-const SafeApp = (): React.ReactElement => {
+import Web3 from 'web3'
+import counterABI from './TestABis/counter.json'
+import messageABI from './TestABis/message.json'
+const provider = new Web3.providers.HttpProvider('https://eth-goerli.g.alchemy.com/v2/Dn8U2J-wzWwQM3EqLryCVFloK9H8OY5q')
+const web3 = new Web3(provider)
+const SafeApp = (): JSX.Element => {
   const { sdk, safe } = useSafeAppsSDK()
 
   const submitTx = useCallback(async () => {
+    function generateTransactionData(
+      contractAddress: string,
+      contractABI: any,
+      functionName: string,
+      functionParameters: any[],
+    ) {
+      const contract = new web3.eth.Contract(contractABI, contractAddress)
+
+      // Get the function object based on the provided function name
+      const functionObject = contract.methods[functionName]
+
+      // Check if the function exists in the contract ABI
+      if (!functionObject) {
+        throw new Error(`Function '${functionName}' not found in the contract ABI.`)
+      }
+
+      // Encode the function call with the provided parameters
+      const functionCallData = functionObject(...functionParameters).encodeABI()
+
+      return functionCallData
+    }
+
     try {
       const { safeTxHash } = await sdk.txs.send({
         txs: [
           {
-            to: safe.safeAddress,
+            to: '0x9ebC361a753Ab4e265fC77cD88940e3f39c5c67B',
             value: '0',
-            data: '0x',
+            data: generateTransactionData('0x9ebC361a753Ab4e265fC77cD88940e3f39c5c67B', counterABI, 'update', [0]),
+          },
+          {
+            to: '0x611D11d216B8cA76B3eab3A2f62D8706d8d1e865',
+            value: '0',
+            data: generateTransactionData('0x611D11d216B8cA76B3eab3A2f62D8706d8d1e865', messageABI, 'updateMessage', [
+              'Hey Ken',
+            ]),
           },
         ],
       })
@@ -40,17 +57,15 @@ const SafeApp = (): React.ReactElement => {
   }, [safe, sdk])
 
   return (
-    <Container>
-      <Title size="md">Safe: {safe.safeAddress}</Title>
+    <div>
+      {safe.safeAddress}
 
-      <Button size="lg" color="primary" onClick={submitTx}>
-        Click to send a test transaction
-      </Button>
+      <button onClick={submitTx}>Click to send a test transaction</button>
 
-      <Link href="https://github.com/safe-global/safe-apps-sdk" target="_blank" rel="noreferrer">
+      {/* <Link href="https://github.com/safe-global/safe-apps-sdk" target="_blank" rel="noreferrer">
         Documentation
-      </Link>
-    </Container>
+      </Link> */}
+    </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-import { ThemeProvider } from 'styled-components'
-import { theme, Loader, Title } from '@gnosis.pm/safe-react-components'
 import SafeProvider from '@safe-global/safe-apps-react-sdk'
 
-import GlobalStyle from './GlobalStyle'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <SafeProvider
-        loader={
-          <>
-            <Title size="md">Waiting for Safe...</Title>
-            <Loader size="md" />
-          </>
-        }
-      >
-        <App />
-      </SafeProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
+  <SafeProvider
+    opts={{
+      allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/],
+      debug: false,
+    }}
+    loader={<>hello</>}
+  >
+    <App />
+  </SafeProvider>,
   document.getElementById('root'),
 )
